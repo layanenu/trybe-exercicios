0ea5d39cb1f6c857d52f3650a3f3b7647e8723ef
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-06-22 11:33:41 -0300

    exercicio 1 resolvido

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,18 @@
+//1 
+const a = 20;
+const b = 10;
+
+const adicao = a + b;
+console.log('A adição entre os elementos a e b é ' + adicao);
+
+const subtracao = a - b;
+console.log('A subtração entre os elementos a e b é ' + subtracao);
+
+const multiplicacao = a * b;
+console.log('A multiplicacao entre os elementos a e b é ' + multiplicacao);
+
+const divisao = a / b;
+console.log('A multiplicacao entre os elementos a e b é ' + multiplicacao);
+
+const modulo = a % b;
+console.log('O modulo entre os elementos a e b é ' + modulo);
