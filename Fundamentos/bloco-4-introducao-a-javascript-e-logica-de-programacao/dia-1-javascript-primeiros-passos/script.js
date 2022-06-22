a2ae0ec5574905612fbb9daee53858e766a46593
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-06-22 12:02:40 -0300

    exercicio 2 resolvido

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,18 +1,45 @@
-//1 
-const a = 20;
-const b = 10;
+//QUESTAO 1 
+// const a = 20;
+// const b = 10;
 
-const adicao = a + b;
-console.log('A adição entre os elementos a e b é ' + adicao);
+// const adicao = a + b;
+// console.log('A adição entre os elementos a e b é ' + adicao);
 
-const subtracao = a - b;
-console.log('A subtração entre os elementos a e b é ' + subtracao);
+// const subtracao = a - b;
+// console.log('A subtração entre os elementos a e b é ' + subtracao);
 
-const multiplicacao = a * b;
-console.log('A multiplicacao entre os elementos a e b é ' + multiplicacao);
+// const multiplicacao = a * b;
+// console.log('A multiplicacao entre os elementos a e b é ' + multiplicacao);
 
-const divisao = a / b;
-console.log('A multiplicacao entre os elementos a e b é ' + multiplicacao);
+// const divisao = a / b;
+// console.log('A multiplicacao entre os elementos a e b é ' + multiplicacao);
 
-const modulo = a % b;
-console.log('O modulo entre os elementos a e b é ' + modulo);
+// const modulo = a % b;
+// console.log('O modulo entre os elementos a e b é ' + modulo);
+
+
+//2
+const primeiroNum = 20;
+const segundoNum = 10;
+
+let resultado;
+
+if (primeiroNum > segundoNum) {
+    resultado = 'O primeiro número é maior do que o segundo número'
+} else if (segundoNum > primeiroNum){
+    resultado ='O segundo número é maior do que o primeiro número'
+}
+console.log(resultado);
+
+// //8
+// const primeiroNum = 3;
+// const segundoNum = 7;
+// const terceiroNum = 9;
+
+// let soma = primeiroNum + segundoNum + terceiroNum
+
+// if (soma % 2 === 0) {
+//     console.log('true')
+// } else {
+//     console.log('false')
+// }
