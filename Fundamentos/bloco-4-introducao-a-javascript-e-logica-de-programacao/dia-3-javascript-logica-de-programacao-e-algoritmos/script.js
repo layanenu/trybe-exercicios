ac9fd85e32d7adcb5d0bf14f74d88368277092f8
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-06-23 18:30:33 -0300

    exercicio 2 resolvido

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,3 +1,4 @@
+//QUESTAO 1
 //1- O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero. Por exemplo:
 //O fatorial é representado pelo sinal !
 //! = 4 x 3 x 2 x 1 = 24
@@ -10,3 +11,22 @@
 // }
 
 // console.log(fatorial);
+//-------------------------------------------
+
+//QUESTAO 2:
+//2-Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para 
+//"ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando 
+//corretamente.
+
+//armazenar a nova palavra invertida = let
+//percorrer a palavra = for
+//uma palavra é tida como um array, então tem que retornar o ultimo indice primeiro
+
+// let word = 'tryber';
+// let palavraInvertida = '';
+// for (let index = 0; index < word.length; index += 1) {
+//     palavraInvertida += word[word.length - 1 - index]; //NAO ENTENDI ESSA PARTE//
+// }
+// console.log(palavraInvertida);
+
+//-------------------------------------------
