cfb08029168124bce38730c33d6494ab02c32b32
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-06-24 12:20:39 -0300

    lista concluida

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -58,24 +58,151 @@
 // }
 
 //QUESTAO 5
-// const primeiroAng = 60;
-// const segundoAng = 60;
-// const terceiroAng = 60;
-// if(primeiroAng + segundoAng + terceiroAng < 180){
-//     console.log('true')
-// } else if (){
-//     console.log('false')
+
+//resolucao em video
+
+// let degreeAngleA = 65;
+// let degreeAngleB = 100;
+// let degreeAngleC = 15;
+
+// let sumOfAngles = degreeAngleA + degreeAngleB + degreeAngleC;
+
+// let allAnglesArePositives = degreeAngleA > 0 && degreeAngleB > 0 && degreeAngleC > 0;
+
+// if(allAnglesArePositives){
+//   if (sumOfAngles === 180) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   };
 // } else {
+//   console.log('Erro: ângulo inválido');
+// }
+
+//QUESTAO 6
+
+//resolucao em video
+
+// const chessPiece = 'bispo';
+
+// switch (chessPiece.toLowerCase()) {
+//   case 'rei':
+//     console.log('Rei-> Uma casa apenas para qualquer direção.');
+//     break;
+//   case 'bispo':
+//     console.log('Bispo-> Diagonal.');
+//     break;
+//   case 'rainha':
+//     console.log('Rainha-> Diagonal, horizontal e vertical.');
+//     break;
+//   case 'cavalo':
+//     console.log('Cavalo -> "L" pode pular sobre as peças.');
+//     break;
+//   case 'torre':
+//     console.log('Torre -> Horizontal e vertical.');
+//     break;
+//   case 'peão':
+//     console.log("Peão -> Apenas uma casa para frente, no primeiro movimento podem ser duas casas.");
+//     break;
+//   default:
+//     console.log('Erro, peça inválida!');
+//     break;
+// };
+
+//QUESTAO 7
+
+// const grade = 76;
+
+// if (grade < 0 || grade > 100) {
+//   console.log("Erro, nota incorreta!");
+// } else if (grade >= 90) {
+//   console.log("A");
+// } else if (grade >= 80) {
+//   console.log("B");
+// } else if (grade >= 70) {
+//   console.log("C");
+// } else if (grade >= 60) {
+//   console.log("D");
+// } else if (grade >= 50) {
+//   console.log("E");
+// } else {
+//   console.log("F");
 // }
 
 
 //QUESTAO 8
-// const primeiroNum = 3;
-// const segundoNum = 7;
-// const terceiroNum = 9;
-// let soma = primeiroNum + segundoNum + terceiroNum
-// if (soma % 2 === 0) {
-//     console.log('true')
+
+//resolucao em video
+
+// const a = 1;
+// const b = 3;
+// const c = 5;
+
+// let isEven = false;
+
+// if ((a % 2 === 0 || b % 2 === 0 || c % 2 === 0)) {
+//   isEven = true;
+// };
+// console.log(isEven);
+
+//QUESTAO 9
+
+// const a = 1;
+// const b = 3;
+// const c = 5;
+
+// let isOdd = false;
+
+// if ((a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0)) {
+//   isOdd = true;
+// };
+// console.log(isOdd);
+
+//QUESTAO 10
+
+// const costOfProduct = 1;
+// const saleValue = 3;
+
+// if (costOfProduct >= 0 && saleValue >= 0) {
+//   const totalCostOfProduct = costOfProduct * 1.2;
+//   const totalProfit = (saleValue - totalCostOfProduct) * 1000;
+//   console.log(totalProfit);
 // } else {
-//     console.log('false')
+//   console.log("Error, os valores não podem ser negativos");
+// };
+
+//QUESTAO 11
+
+//resolucao em video
+
+// let aliquotINSS;
+// let aliquotIR;
+
+// const grossSalary = 2000.00;
+
+// if (grossSalary <= 1556.94) {
+//   aliquotINSS = grossSalary * 0.08;
+// } else if (grossSalary <= 2594.92) {
+//   aliquotINSS = grossSalary * 0.09;
+// } else if (grossSalary <= 5189.82) {
+//   aliquotINSS = grossSalary * 0.11;
+// } else {
+//   aliquotINSS = 570.88;
 // }
+
+// const baseSalary = grossSalary - aliquotINSS;
+
+// if (baseSalary <= 1903.98) {
+//   aliquotIR = 0;
+// } else if (baseSalary <= 2826.65) {
+//   aliquotIR = (baseSalary * 0.075) - 142.80;
+// } else if (baseSalary <= 3751.05) {
+//   aliquotIR = (baseSalary * 0.15) - 354.80;
+// } else if (baseSalary <= 4664.68) {
+//   aliquotIR = (baseSalary * 0.225) - 636.13;
+// } else {
+//   aliquotIR = (baseSalary * 0.275) - 869.36;
+// };
+
+// console.log("Salário: " + (baseSalary - aliquotIR));
+
