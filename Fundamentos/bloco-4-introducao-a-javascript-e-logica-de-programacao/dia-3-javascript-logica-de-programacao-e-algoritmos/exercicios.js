839f26b462e5f2108d3768137b2d081be2d67e4e
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-06-24 12:48:36 -0300

    lista concluida

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -1,59 +1,56 @@
 //QUESTAO 1
-//1- O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero. Por exemplo:
-//O fatorial é representado pelo sinal !
+
 //! = 4 x 3 x 2 x 1 = 24
-//Com base nessas informações, crie um algoritmo que retorne o fatorial de 10.
+
 // let fatorial = 1;
+
 // for (let index = 10; index > 0; index -= 1) {
 //   fatorial *= index;
 // }
+
 // console.log(fatorial);
-//-------------------------------------------
 
 //QUESTAO 2:
-//2-Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para 
-//"ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando 
-//corretamente.
 // let word = 'tryber';
-// let palavraInvertida = '';
+// let reverseWord = '';
+
 // for (let index = 0; index < word.length; index += 1) {
-//     palavraInvertida += word[word.length - 1 - index];
+//   reverseWord += word[word.length - 1 - index];
 // }
-// console.log(palavraInvertida);
 
-//-------------------------------------------
+// console.log(reverseWord);
+
+//outra solucao:
+
+// let word = 'tryber';
+// let reverseWord = '';
+
+// reverseWord = word.split('').reverse().join('');
+
+// console.log(reverseWord);
+
 //QUESTAO 3:
-//3- Considere o array de strings abaixo:
-//Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de 
-//caracteres de cada palavra.
-
 // let array = ['java', 'javascript', 'python', 'html', 'css'];
 
-// let min;
-// let max;
+// let biggestWord = array[0];
+// let smallestWord = array[0];
 
-// for(index = 0; index < array.length; index += 1) {
-//     if (index === 0) {  
-//         min = array[index]
-//         max = array[index]
-//     } else {
-//         if (max.length > array[index].length){
-//             max = array[index]
-//         }
-
-//         if (min.length < array[index].length){
-//             min = array[index]
-//         }
-//     }
+// for (let index = 0; index < array.length; index += 1) {
+//   if (array[index].length > biggestWord.length) {
+//     biggestWord = array[index];
+//   }
 // }
 
-// console.log(max,min);
+// for (let index = 0; index < array.length; index += 1) {
+//   if (array[index].length < smallestWord.length) {
+//     smallestWord = array[index];
+//   }
+// }
 
-//-------------------------------------------
+// console.log(biggestWord);
+// console.log(smallestWord);
+
 //QUESTAO 4:
-//4- Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele 
-//mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 2 e 50.
-
 // let biggestPrimeNumber = 0;
 
 // for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
@@ -69,9 +66,3 @@
 // }
 
 // console.log(biggestPrimeNumber);
-
-//-------------------------------------------
-//BONUS:
-//1- Agora vamos trabalhar com algumas formas geométricas! Faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela 
-//um quadrado feito de asteriscos de lado de tamanho n. Por exemplo:
-//
