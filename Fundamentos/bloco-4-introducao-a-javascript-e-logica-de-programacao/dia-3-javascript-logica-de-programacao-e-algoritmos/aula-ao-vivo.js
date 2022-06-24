9f163bbe2d5c41102f07a5e841c2501577ded1af
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-06-24 10:11:11 -0300

    exercicio 3 resolvido

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/aula-ao-vivo.js
@@ -0,0 +1,20 @@
+// Nosso desafio
+// Você deve criar um sistema que permita o cadastro de novos usuários nos sistemas da empresa, gerar senhas para todos os novos 
+// usuários. Inserir os novos usuários nos sistemas da empresa.
+
+let novosUsuarios = [
+    'Marcos',
+    'Camila',
+    'Adriana',
+    'Karla',
+    'José',
+    'Marcia',
+    'Michelle',
+    'Michel',
+];
+
+// Requisitos
+// 1 - O sistema deve fazer um controle de acesso e só liberar a execução das atividades se a pessoa usuária fizer login no sistema 
+// e tiver acesso liberado para realizar essas operações (Login: 'usuarioTrybe' com a senha: 'trybe2022'); 
+// Se a pessoa usuária tiver acesso, o sistema deve imprimir a mensagem 'Acesso permitido!', caso contrário a mensagem exibida
+// será 'Acesso negado!'
--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -3,13 +3,10 @@
 //O fatorial é representado pelo sinal !
 //! = 4 x 3 x 2 x 1 = 24
 //Com base nessas informações, crie um algoritmo que retorne o fatorial de 10.
-
 // let fatorial = 1;
-
 // for (let index = 10; index > 0; index -= 1) {
 //   fatorial *= index;
 // }
-
 // console.log(fatorial);
 //-------------------------------------------
 
@@ -17,16 +14,35 @@
 //2-Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para 
 //"ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando 
 //corretamente.
-
-//armazenar a nova palavra invertida = let
-//percorrer a palavra = for
-//uma palavra é tida como um array, então tem que retornar o ultimo indice primeiro
-
 // let word = 'tryber';
 // let palavraInvertida = '';
 // for (let index = 0; index < word.length; index += 1) {
-//     palavraInvertida += word[word.length - 1 - index]; //NAO ENTENDI ESSA PARTE//
+//     palavraInvertida += word[word.length - 1 - index];
 // }
 // console.log(palavraInvertida);
 
 //-------------------------------------------
+//QUESTAO 3:
+//3- Considere o array de strings abaixo:
+//Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de 
+//caracteres de cada palavra.
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// let biggestWord = array[0];
+// let smallestWord = array[0];
+
+// for (let index = 0; index < array.length; index += 1) {
+//   if (array[index].length > biggestWord.length) {
+//     biggestWord = array[index];
+//   }
+// }
+
+// for (let index = 0; index < array.length; index += 1) {
+//   if (array[index].length < smallestWord.length) {
+//     smallestWord = array[index];
+//   }
+// }
+
+// console.log(biggestWord);
+// console.log(smallestWord);
