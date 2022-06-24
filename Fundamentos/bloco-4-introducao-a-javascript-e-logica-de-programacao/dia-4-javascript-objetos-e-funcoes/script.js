f3176980c031066a790fcf41baadf9b06de7e63d
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-06-24 15:44:45 -0300

    questao 3 resolvida

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -12,11 +12,16 @@ let player = {
     medals: {
         golden: 2,
         silver: 3
-    }
-}
+    },
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+};
 
 //2 - Acesse as chaves name, lastName e age e concatene as suas informações para imprimir no console uma mensagem no seguinte formato: 
 //"A jogadora Marta Silva tem 34 anos de idade".
 
 console.log('A jogadora '+ player.name + ' ' + player.lastName + ' tem ' + player.age + ' ' + 'de idade.')
 console.log('A jogadora '+ player['name'] + ' ' + player['lastName'] + ' tem ' + player['age'] + ' ' + 'de idade.')
+
+//3 - Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada 
+//a melhor do mundo. [2006, 2007, 2008, 2009, 2010, 2018]
+
