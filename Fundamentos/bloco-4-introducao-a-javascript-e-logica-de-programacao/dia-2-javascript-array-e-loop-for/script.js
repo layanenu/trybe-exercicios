24ef7dd37d6075a633dce05c106decaada8469fc
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-06-24 12:39:35 -0300

    lista concluida

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,34 +1,125 @@
-//1-Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+//Iremos utilizar esse array nos exercícios:
 
 // let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// for(let index = 0; index < numbers.length; index +=1) {
-//     console.log(numbers[index]);
-// }
 
-//2-Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+//QUESTAO 1
 
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// let resultado = 0;
-// for (let index = 0; index < numbers.length; index += 1) {
-//     resultado += numbers[index];
-// }
-// console.log(resultado;)
-
-//3-Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
-//A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
+//Resolucao em video
 
 // let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// let resultado = 0;
-// let media = 0;
 
 // for (let index = 0; index < numbers.length; index += 1) {
-//     resultado += numbers[index];
+//   console.log(numbers[index]);
 // }
 
-const n = 9;
-let resultado = 100;
-for (let i =0; i <= n; i += 1) {
-    resultado -= i;
-}
+//QUESTAO 2
 
-console.log(resultado);
+//Resolucao em video
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let result = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   result += numbers[index];
+// }
+
+// console.log(result);
+
+//QUESTAO 3
+
+//Resolucao em video
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   sum += numbers[index];
+// }
+
+// let average = sum / numbers.length;
+
+// console.log(average);
+
+//QUESTAO 4
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let result = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   result += numbers[index];
+// }
+
+// result = result / numbers.length;
+
+// if (result > 20) {
+//   console.log('valor maior que 20');
+// } else {
+//   console.log('valor menor ou igual a 20');
+// }
+
+//QUESTAO 5
+
+//Resolucao em video
+
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// o valor da variável higherNumber poderia ser qualquer um, desde que fosse menor que o menor número do array numbers. Caso atribuíssemos o valor 101 para a variável, nosso algoritmo estaria errado, pois ele nunca acharia um número maior que 101 no array. Para resolver esse problema vamos iniciar a variavel com o primeiro valor do array. Dessa forma podemos pular a posição 0 e iniciar com index = 1
+
+// let higherNumber = numbers[0];
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//   if (numbers[index] > higherNumber) {
+//     higherNumber = numbers[index];
+//   }
+// }
+
+// console.log(higherNumber);
+
+//QUESTAO 6
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let result = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] % 2 !== 0) {
+//     result += 1;
+//   }
+// }
+
+// if (result === 0) {
+//   console.log('nenhum valor ímpar encontrado');
+// } else {
+//   console.log(result);
+// }
+
+//QUESTAO 7
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// // o valor da variável smallestNumber poderia ser qualquer um, desde que fosse maior que o maior número do array numbers. Caso atribuíssemos o valor 1 para a variável, nosso algoritmo estaria errado, pois ele nunca acharia um número menor que 1 no array. Para resolver esse problema vamos iniciar a variavel com o primeiro valor do array. Dessa forma podemos pular a posição 0 e iniciar com index = 1
+
+// let smallestNumber = numbers[0];
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//   if (numbers[index] < smallestNumber) {
+//     smallestNumber = numbers[index];
+//   }
+// }
+
+// console.log(smallestNumber);
+
+//QUESTAO 8
+
+// let numbers = [];
+
+// for (let index = 1; index <= 25; index += 1) {
+//   numbers.push(index);
+// }
+
+// console.log(numbers);
+
+//QUESTAO 9
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     console.log(numbers[index] / 2);
+//   };
