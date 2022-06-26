2639300712801fae616e8e43c233794a41fc327e
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-06-26 15:37:18 -0300

    exercicio 6 resolvido Parte I - Objetos e For/In

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.parte-I-objetos-e-for-in.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.parte-I-objetos-e-for-in.js
@@ -1,15 +1,15 @@
 //1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
 
     // console.log('Bem vinda ' + info.personagem);
     // console.log('Bem vinda ' + info['personagem']);
 
 //2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
-info['recorrente'] = "Sim";
+// info['recorrente'] = "Sim";
 
 // console.log(info);
 
@@ -21,29 +21,47 @@ info['recorrente'] = "Sim";
 
 //4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
 
-for (let key in info){
-    console.log(info[key]);
-}
+// for (let key in info){
+//     console.log(info[key]);
+// }
 
 //5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 
 //'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto 
 //juntos de acordo com cada uma das chaves.
 
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim',
-};
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim',
+// };
   
-  for (let properties in info) {
-    if (
-      properties === 'recorrente' &&
-      info[properties] === 'Sim' &&
-      info2[properties] === 'Sim'
-    ) {
-      console.log('Ambos recorrentes');
-    } else {
-      console.log(info[properties] + ' e ' + info2[properties]);
-    }
-  }
+//   for (let properties in info) {
+//     if (
+//       properties === 'recorrente' &&
+//       info[properties] === 'Sim' &&
+//       info2[properties] === 'Sim'
+//     ) {
+//       console.log('Ambos recorrentes');
+//     } else {
+//       console.log(info[properties] + ' e ' + info2[properties]);
+//     }
+//   }
+
+// 🚀 6 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: 
+//"O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo);
