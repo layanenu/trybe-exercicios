6482817f1522b2da34c815add3b5018300e9ab38
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-06-26 15:18:41 -0300

    exercicio 4 resolvido Parte I - Objetos e For/In

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.parte-I-objetos-e-for-in.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.parte-I-objetos-e-for-in.js
@@ -15,6 +15,12 @@ info['recorrente'] = "sim";
 
 //3 - Faça um for/in que mostre todas as chaves do objeto. 
 
+// for (let key in info){
+//     console.log(key);
+// }
+
+//4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+
 for (let key in info){
-    console.log(key);
+    console.log(info[key]);
 }
