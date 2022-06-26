2383b4cee83ba9b556dda0f76437533446a0b61b
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-06-26 15:41:54 -0300

    exercicio 7 resolvido Parte I - Objetos e For/In

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.parte-I-objetos-e-for-in.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.parte-I-objetos-e-for-in.js
@@ -64,4 +64,16 @@ let leitor = {
     ],
   };
 
-console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo);
+// console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo);
+
+//🚀 7 - Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a esta chave um objeto contendo as seguintes 
+//informações:
+
+leitor.livrosFavoritos.push (
+    {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editora: 'Rocco',
+      }
+)
+console.log(leitor);
