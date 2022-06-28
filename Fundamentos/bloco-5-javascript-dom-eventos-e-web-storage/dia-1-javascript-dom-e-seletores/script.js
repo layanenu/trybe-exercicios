f02ae82091ef71f170c300817b5d4c775dce5364
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-06-28 15:57:49 -0300

    questao 1 resolvida do exercicio para fixar getelementbyid

---
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,4 @@
+document.getElementById("page-title").innerText = "Meu filme favorito do momento é Red: crescer é uma fera";
+
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
