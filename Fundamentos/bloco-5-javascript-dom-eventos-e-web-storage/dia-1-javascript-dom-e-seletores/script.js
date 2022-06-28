3e926dab7ea385bee646b6de5f2782e098f1be03
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-06-28 16:03:24 -0300

    questao 3 resolvida do exercicio para fixar getElementById

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -4,3 +4,5 @@ const paragraph = document.getElementById("paragraph");
 paragraph.style.color = "red";
 
 document.getElementById("second-paragraph").innerText = "O filme fala sobre uma menina de 13 anos começa a se transformar em um panda vermelho gigante sempre que fica animada.";
+
+document.getElementById("subtitle").innerText = "Alguns personagens: Meilin Lee, Robaire, Miriam Mendelsohn...";
