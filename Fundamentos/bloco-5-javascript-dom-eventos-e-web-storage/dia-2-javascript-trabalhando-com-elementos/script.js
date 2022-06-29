9898d59536a1077ed1ab2938f0effffa8f30f658
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-06-29 15:30:18 -0300

    parte I questao 3 resolvida

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -8,3 +8,5 @@ const pai = elementoOndeVoceEsta.parentElement;
 pai.style.color = 'blue';
 
 //3. Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+
+const primeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho').innerText = 'Primeiro Filho Do Filho';
