bc533f39a8862d7e553a5deb972eb8a195561eea
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-06-29 15:22:14 -0300

    parte I questao 2 resolvida

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -1,3 +1,10 @@
 // 1. Acesse o elemento elementoOndeVoceEsta.
 
-const elementoOndeVoceEsta =document.querySelector('#elementoOndeVoceEsta');
+const elementoOndeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
+
+// 2. Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+
+const pai = elementoOndeVoceEsta.parentElement;
+pai.style.color = 'blue';
+
+//3. Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
