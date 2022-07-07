1eb4cd06e7f4c1ec292fcd190151fbf578e29671
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-07-07 12:00:54 -0300

    Parte IV - questao 1 resolvida

---
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,10 @@
+// SELETORES
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+function teste(event){
+  event.preventDefault();
+}
+
+HREF_LINK.addEventListener('click', teste);
